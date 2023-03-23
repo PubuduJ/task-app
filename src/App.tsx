f4a3aed34c064c73e60134193d1f1091eef9e0dc
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-23 11:33:32 +0530

    Complete initial markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Task} from "./types";
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+  const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([])
+    return (
+        <div className={"app"}>
+            <span className={"heading"}>Task App</span>
+        </div>
+    );
 }
 
 export default App;
