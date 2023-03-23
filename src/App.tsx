46592b2a5bee65e1f4fdfdfc7613c0aa5be7dd99
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-23 11:42:41 +0530

    Create InputField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React, {useState} from 'react';
 import {Task} from "./types";
+import InputField from "./components/InputField";
 
 function App() {
-  const [task, setTask] = useState<string>("");
-  const [tasks, setTasks] = useState<Task[]>([])
+    const [task, setTask] = useState<string>("");
+    const [tasks, setTasks] = useState<Task[]>([])
+
+    function handleAdd(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        if (task) {
+            setTasks([...tasks, {
+                id: Date.now(),
+                task: task,
+                isDone: false
+            }]);
+            setTask("");
+        }
+    }
+
     return (
         <div className={"app"}>
             <span className={"heading"}>Task App</span>
+            <InputField task={task} setTask={setTask} handleAdd={handleAdd}/>
         </div>
     );
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,28 @@
+import React, {SetStateAction, useRef} from "react";
+
+type InputFieldProps = {
+    task: string;
+    setTask: React.Dispatch<SetStateAction<string>>;
+    handleAdd: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+function InputField({task, setTask, handleAdd}: InputFieldProps) {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const newTask = event.target.value;
+        setTask(newTask);
+    }
+
+    return (
+        <form className={"input"} onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+            handleAdd(event);
+            inputRef.current?.blur();
+        }}>
+            <input ref={inputRef} className={"input-box"} type={"text"} placeholder={"Enter a task"} onChange={handleChange} value={task}/>
+            <button className={"input-submit"} type={"submit"}>Go</button>
+        </form>
+    );
+}
+
+export default InputField;
